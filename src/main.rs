21400c05acffa55d3f9d9a53770c52c7142abdb9
Chris Pearce <chris@pearce.org.nz>
2017-08-06 15:11:36 +1200

    Builds and runs, but untested.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::fs::File;
 use std::collections::HashMap;
 use std::hash::{Hash, Hasher};  
 
+
 fn count_item_frequencies(itemizer: &mut Itemizer, path: &str) -> Result<HashMap<u32, u32>, Box<Error>> {
     // Build the CSV reader and iterate over each record.
     let mut item_count: HashMap<u32, u32> = HashMap::new();
@@ -24,58 +25,94 @@ fn count_item_frequencies(itemizer: &mut Itemizer, path: &str) -> Result<HashMap
     Ok(item_count)
 }
 
-// #[derive(Hash, Eq, PartialEq, Debug)]
-// Parent node must outlive child nodes.
-struct FPNode<'a> {
-    parent: Option<&'a FPNode<'a>>,
+#[derive(Eq, PartialEq, Debug)]
+struct FPNode {
     item: u32,
     count: u32,
     end_count: u32,
-    children: HashMap<u32, FPNode<'a>>,
+    children: HashMap<u32, FPNode>,
 }
 
-impl<'a> Hash for FPNode<'a> {
+impl Hash for FPNode {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.item.hash(state);
     }
 }
 
-impl <'a> FPNode<'a> {
-    fn insert(&mut self, transaction: &[u32]) {
+// impl Eq for FPNode {
+//     fn eq(&self, other: &FPNode) -> bool {
+//         self.item == other.item;
+//     }
+// }
 
-    }
+struct FPTree {
+    root: FPNode,
+    num_transactions: u32,
+    item_count: HashMap<u32, u32>,
 }
 
-struct FPTree<'a> {
-    root: FPNode<'a>,
-}
-
-impl <'a> FPTree<'a> {
-
-    fn new() -> FPTree<'a> {
-        let root_node = FPNode {
-            parent: None,
-            item: 0,
+impl FPNode {
+    fn new(item: u32) -> FPNode {
+        FPNode {
+            item: item,
             count: 0,
             end_count: 0,
             children: HashMap::new()
-        };
-        return FPTree{ root: root_node };
+        }
     }
 
-    fn insert(&mut self, transaction: &[u32]) {
-        let mut node = &mut self.root;
-        for item in transaction {
-            if let Some(child) = node.children.get(item) {
-                node = child;
-                node.count += 1;
-                continue;
-            }
-            // node.children.insert(item,)
-            // let mut child
+    fn insert(&mut self, transaction: &[u32], count: u32) {
+        if transaction.len() == 0 {
+            return;
         }
-        
+        // if transaction.len()
+        let item = transaction[0];
+        let mut child = self.children.entry(item).or_insert(FPNode::new(item));
+        child.count += 1;
+        if transaction.len() == 1 {
+            child.end_count += count;
+        } else {
+            child.insert(&transaction[1 ..], count);
+        }
     }
+    fn is_root(&self) -> bool {
+        self.item == 0
+    }
+}
+
+impl FPTree {
+
+    fn new() -> FPTree {
+        let root_node = FPNode::new(0);
+        return FPTree {
+            root: root_node,
+            num_transactions: 0,
+            item_count: HashMap::new()
+        };
+    }
+
+    fn insert(&mut self, transaction: &[u32], count: u32) {
+        // Keep a count of item frequencies of what's in the
+        // tree to make sorting later easier.
+        for item in transaction {
+            *self.item_count.entry(*item).or_insert(0) += count;
+        }
+        self.root.insert(&transaction, count);       
+        self.num_transactions += 1; 
+    }
+
+    fn num_transactions(&self) -> u32 {
+        self.num_transactions
+    }
+
+    fn root(&self) -> &FPNode {
+        &self.root
+    }
+
+    fn item_count(&self) -> &HashMap<u32, u32> {
+        &self.item_count
+    }
+
 }
 
 
@@ -134,12 +171,122 @@ fn sort_transaction(transaction: &mut [u32],
 }
 
 
+fn add_parents_to_table<'a>(node: &'a FPNode, table: &mut HashMap<&'a FPNode, &'a FPNode>) {
+    for child in node.children.values() {
+        table.insert(child, node);
+        add_parents_to_table(child, table)
+    }
+}
+
+fn make_parent_table<'a>(fptree: &'a FPTree) -> HashMap<&'a FPNode, &'a FPNode> {
+    let mut table = HashMap::new();
+    add_parents_to_table(fptree.root(), &mut table);
+    table
+}
+
+fn add_nodes_to_index<'a>(node: &'a FPNode,
+                          index: &mut HashMap<u32, Vec<&'a FPNode>>) {
+    for child in node.children.values() {
+        index.entry(child.item).or_insert(vec![]).push(&child);
+        add_nodes_to_index(child, index)
+    }
+}
+
+fn make_item_index<'a>(fptree: &'a FPTree) -> HashMap<u32, Vec<&'a FPNode>> {
+    let mut index = HashMap::new();
+    add_nodes_to_index(fptree.root(), &mut index);
+    index
+}
+
+fn path_from_root_to<'a>(node: &'a FPNode,
+                    parent_table: &HashMap<&'a FPNode, &'a FPNode>)
+                    -> Vec<u32> {
+    let mut path = vec![];
+    let mut n = node;
+    loop {
+        match parent_table.get(n) {
+            Some(parent) if !parent.is_root() => {
+                path.push(parent.item);
+                n = parent;
+                continue;
+            },
+            _ => { break; }
+        }
+    }
+    path.reverse();
+    path
+}
+
+fn construct_conditional_tree<'a>(fptree: &'a FPTree,
+                                  item: u32,
+                                  parent_table: &HashMap<&'a FPNode, &'a FPNode>,
+                                  item_list: &Vec<&'a FPNode>) -> FPTree {
+    let mut conditional_tree = FPTree::new();
+
+    for node in item_list {
+        let path = path_from_root_to(node, parent_table);
+        conditional_tree.insert(&path, node.count);
+    }
+    // let paths = item_index.g
+    conditional_tree
+}
+
+fn fp_growth(fptree: &FPTree, min_count: u32, path: &[u32]) -> Vec<Vec<u32>> {
+    // println!("fpgrowth path={:?}", path);
+    let mut itemsets = vec![];
+
+    // Maps a node to its parent.
+    let parent_table = make_parent_table(&fptree);
+    
+    // Maps item id to vec of &FPNode's for those items.
+    let item_index = make_item_index(&fptree);
+    
+    // Get list of items in the tree which are above the minimum support
+    // threshold. Sort the list in increasing order of frequency.
+    let mut items: Vec<u32> =
+        item_index.keys()
+                  .map(|x| *x)
+                  .filter(|x| get_item_count(*x, fptree.item_count()) > min_count)
+                  .collect();
+    sort_transaction(&mut items, fptree.item_count());
+    items.reverse();
+
+    // println!("fp_growth items {:?}", items);
+
+    for item in items {
+        // The path to here plus this item must be above the minimum
+        // support threshold.
+        let mut itemset: Vec<u32> = Vec::from(path);
+        itemset.push(item);
+
+        let item_list = if let Some(item_list) = item_index.get(&item) {
+            let conditional_tree =
+                construct_conditional_tree(&fptree,
+                                           item,
+                                           &parent_table,
+                                           item_list);
+            itemsets.extend(fp_growth(&conditional_tree, min_count, &itemset));
+        };
+
+        itemsets.push(itemset);
+
+    }
+
+    itemsets
+}
+
 fn run(path: &str) -> Result<(), Box<Error>> {
+    println!("Entering run()");
+
+    // Make one pass of the dataset to calculate the item frequencies
+    // for the initial tree.
     let mut itemizer: Itemizer = Itemizer::new();
     let item_count = count_item_frequencies(&mut itemizer, path).unwrap();
 
-    let mut fptree = FPTree::new();
+    println!("Counted item frequencies.");
 
+    // Load the initial tree.
+    let mut fptree = FPTree::new();
     // For each record in file...
     let mut file = File::open(path)?;
     let mut rdr = csv::Reader::from_reader(file);
@@ -155,15 +302,21 @@ fn run(path: &str) -> Result<(), Box<Error>> {
         for i in 1..transaction.len() {
             assert!(get_item_count(transaction[i - 1], &item_count) >= get_item_count(transaction[i], &item_count));
         }
-        fptree.insert(&transaction);
+        fptree.insert(&transaction, 1);
     }
 
+    println!("Starting mining");
+
+    // let min_support = 0.2;
+    let min_count = fptree.num_transactions() / 5;
+
+    let patterns = fp_growth(&fptree, min_count, &vec![]);
 
     Ok(())
 }
 
 fn main() {
-    if let Err(err) = run("c:\\Users\\chris\\src\\rust\\arm\\datasets\\UCI-zoo.csv") {
+    if let Err(err) = run("c:\\Users\\chris\\src\\rust\\arm\\datasets\\test.csv") {
         println!("error running example: {}", err);
         process::exit(1);
     }
