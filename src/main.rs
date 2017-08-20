8375e750d8796ebadf7ee83b5c42947cd72e8ad4
Chris Pearce <chris@pearce.org.nz>
2017-08-20 22:05:08 +1200

    Add logging of run time.

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use std::io::BufReader;
 use std::io::prelude::*;
 use std::path::Path;
 use std::process;
+use std::time::Instant;
 
 
 struct TransactionReader<'a> {
@@ -349,26 +350,34 @@ fn fp_growth(fptree: &FPTree, min_count: u32, path: &[u32], itemizer: &Itemizer)
 fn mine_fp_growth(input_csv_file_path: &str,
                   output_csv_file_path: &str,
                   min_support: f64) -> Result<(), Box<Error>> {
+    println!("Mining data set: {}", input_csv_file_path);
     println!("Making first pass of dataset to count item frequencies...");
     // Make one pass of the dataset to calculate the item frequencies
     // for the initial tree.
+    let start = Instant::now();
+    let timer = Instant::now();
     let mut itemizer: Itemizer = Itemizer::new();
     let item_count = count_item_frequencies(
         TransactionReader::new(input_csv_file_path, &mut itemizer)).unwrap();
+    println!("First pass took {} seconds.", timer.elapsed().as_secs());
 
     println!("Building initial FPTree based on item frequencies...");
 
     // Load the initial tree, by re-reading the data set and inserting
     // each transaction into the tree sorted by item frequency.
+    let timer = Instant::now();
     let mut fptree = FPTree::new();
     for mut transaction in TransactionReader::new(input_csv_file_path, &mut itemizer) {
         sort_transaction(&mut transaction, &item_count, SortOrder::Decreasing);
         fptree.insert(&transaction, 1);
     }
+    println!("Building initial FPTree took {} seconds.", timer.elapsed().as_secs());
 
     println!("Starting recursive FPGrowth...");
+    let timer = Instant::now();
     let min_count = (min_support * (fptree.num_transactions() as f64)) as u32;
     let patterns: Vec<Vec<u32>> = fp_growth(&fptree, min_count, &vec![], &itemizer);
+    println!("FPGrowth took {} seconds.", timer.elapsed().as_secs());
 
     // Convert frequent itemsets from a vector of item ids to
     // vector of friendly string item names.
@@ -378,9 +387,18 @@ fn mine_fp_growth(input_csv_file_path: &str,
     };
 
     // Output the itemsets.
-    for itemset in patterns.iter().map(u32_vec_to_string_vec) {
-        println!("{:?}", itemset);
+    let timer = Instant::now();
+    {
+        println!("FPGrowth complete, generated {} frequent itemsets.", patterns.len());
+        println!("Writing frequent itemsets to output file...");
+        let mut output = File::create(output_csv_file_path)?;
+        for itemset in patterns.iter().map(u32_vec_to_string_vec) {
+            writeln!(output, "{}", itemset.join(","))?;
+        }
     }
+    println!("Writing frequent itemsets took {} seconds", timer.elapsed().as_secs());
+
+    println!("Total runtime: {} seconds", start.elapsed().as_secs());
 
     Ok(())
 }
